7efd0619709ed3cbfeec5f872b705fca9e2ca1fa
Jan Góral <jan.goral112@gmail.com>
2021-03-15 18:24:03 +0100

    testy_v1 Added test shouldFindElementInSingleElementArray in BinarySearchTest

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
 class BinarySearchTest {
 
     @BeforeEach
@@ -14,6 +13,24 @@ class BinarySearchTest {
     @Test
     void test() {
         fail("Not yet implemented");
+    }
+
+    @Test
+    void shouldFindElementInSingleElementArray() {
+        // given
+        int key = 12;
+        int[] sequence = {12};
+
+        boolean expectedFindStatus = true;
+        int expectedPosition = 0;
+
+        // when
+        SearchResult searchResult = BinarySearch.search(key, sequence);
+
+        // then
+        assertEquals(expectedFindStatus, searchResult.isFound());
+        assertEquals(expectedPosition, searchResult.getPosition());
+        assertEquals(key, sequence[searchResult.getPosition()]);
     }
 
 }
